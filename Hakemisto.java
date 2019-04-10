af7bc1a2c390edb6647cdea685d79c9752631e3a
Helen Konttinen <helen.konttinen@tuni.fi>
2019-04-10 15:53:22 +0300

    Projektin oma aloitus. Lisätty harjoitustyo.tiedot-pakkaus.

---
+++ Hakemisto.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package harjoitustyo.tiedot;
+
+import harjoitustyo.apulaiset.Sailova;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author helen
+ */
+public class Hakemisto<T> extends Tieto implements Sailova<T> {
+    /**
+     * Attribuutit.
+     * */
+    OmaLista<Tieto> sisalto;
+    
+    private Hakemisto ylihakemisto;
+
+    /**
+     * Rakentajat.
+     * */
+    public Hakemisto() {
+        this.sisalto = new OmaLista<>();
+        ylihakemisto = null;
+    }
+    
+    /**
+     * 
+     * @param hakemistoNimi
+     * @param yHakemisto
+     */
+    public Hakemisto (StringBuilder hakemistoNimi, Hakemisto yHakemisto)throws IllegalArgumentException {
+        super(hakemistoNimi);
+        ylihakemisto(yHakemisto);
+        this.sisalto = new OmaLista<>();
+    }
+    
+    /**
+     * Aksessorit.
+     * @param yHakemisto
+     */
+    private void ylihakemisto(Hakemisto yHakemisto) {
+        ylihakemisto = yHakemisto;
+    }
+    
+    /**
+     * Korvatut metodit.
+     * */
+    @Override
+    public LinkedList hae(String hakusana) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean lisaa(Object lisattava) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean poista(Object poistettava) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * Object-luokan korvatut metodit.
+     */    
+    @Override
+    public String toString () {
+        return super.toString() + " ";
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-## Projektipohja
-
-Työhakemistossa on valmiiksi vain _README.md_.
